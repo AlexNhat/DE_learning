0d84fac8ba8a73f9341b4cf0931b192a08e10679
NhatLNA <anhnhat909940@gmail.com>
2025-11-12 16:59:24 +0700

    config duckdb

---
+++ sandbox/duckdb/config.py
@@ -0,0 +1,7 @@
+python_enable_replacements = False
+enable_external_access = True
+preserve_insertion_order = False
+threads = 4
+data_size = 1000000
+memory_limit = "1GB"
+enable_progress_bar = True
